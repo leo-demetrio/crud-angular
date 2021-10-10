524f6cd0b836a744cedd7a9856eb4bca59db6251
leo-demetrio <leocdemetrio@gmail.com>
2021-10-10 20:22:19 -0300

    feat-2: created function create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
-
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 const routes: Routes = [
   {
     path: "",
@@ -12,7 +12,12 @@ const routes: Routes = [
   {
     path: "products",
     component: ProductCrudComponent
-  }
+  },
+  {
+    path: "products/create",
+    component: ProductCreateComponent
+  },
+  
 ];
 
 @NgModule({
