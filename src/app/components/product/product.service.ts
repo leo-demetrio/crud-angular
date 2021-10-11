8d929a120dbd1a9c6008d5f78df756e1e267f722
leo-demetrio <leocdemetrio@gmail.com>
2021-10-11 16:49:27 -0300

    feat-4: delete created

--- src/app/components/product/product.service.ts
+++ src/app/components/product/product.service.ts
@@ -31,7 +31,7 @@ export class ProductService {
   read(): Observable<Product[]> {
     return this.http.get<Product[]>(this.baseUrl);
   }
-  
+
   readById(id: string): Observable<Product> {
     const url = `${this.baseUrl}/${id}`;
     return this.http.get<Product>(url);
@@ -40,5 +40,9 @@ export class ProductService {
   update(product: Product): Observable<Product> {
     const url = `${this.baseUrl}/${product.id}`;
     return this.http.put<Product>(url, product);
+  }
+  delete(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.delete<Product>(url);
   }
 }
